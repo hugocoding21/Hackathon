9ba5238807ebf576b3b7815af4ebd139b3c1ef5f
hugocoding21 <hhanocq@gmail.com>
2023-05-11 15:00:23 +0200

    Page home

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import "./App.css";
+import Filter from "./assets/components/FilterDestination/Filter";
+import React from "react";
 
 function App() {
-  return <></>;
+  return (
+    <div className="page">
+      <Filter />
+    </div>
+  );
 }
 
 export default App;
