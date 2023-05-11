66d339b3bd6a0ff64ba28928710bcc59a08b7401
Khouloud Belkhir <koukabelkhir@gmail.com>
2023-05-11 17:12:27 +0200

    pop-up added

--- src/assets/components/Listing.jsx
+++ src/assets/components/Listing.jsx
@@ -1,32 +1,57 @@
 import React, { useState } from "react";
-import {houses} from "./Listings"; 
-import "./Listing.scss"; 
+import { houses } from "./Listings";
+import "./Listing.scss";
 import house from "./house.jpg";
 
 function Listing() {
-const openModal = () => {
-    return (
-        <></>
-    );
-}
-  return <>
-  <div className="Listings">
-      {
-         houses.map((data, key) => {
-            return (
-              <div key={key} className="house"
-              onClick={openModal}>
-                 <img src={house} alt=""/>
-                <p>{data.description}</p>
-               
-              </div>
-            );
-          })
-      }  
+  const [selectedHouse, setSelectedHouse] = useState(null);
+  const [showModal, setShowModal] = useState(false);
 
+  const openModal = (house) => {
+    setSelectedHouse(house);
+    setShowModal(true);
+  };
 
-    </div>  
-  </>;
+  const closeModal = () => {
+    setSelectedHouse(null);
+    setShowModal(false);
+  };
+
+  return (
+    <>
+      <div className="Listings">
+        {houses.map((data, key) => {
+          return (
+            <div key={key} className="house" onClick={() => openModal(data)}>
+              <img src={house} alt="" />
+              <p>{data.description}</p>
+            </div>
+          );
+        })}
+      </div>
+
+      {selectedHouse && showModal && (
+        <div className="modal" onClick={closeModal}>
+          <div className="modal-content">
+            <h2>Book your house in {selectedHouse.city}</h2>
+            <p className="home_description">{selectedHouse.description}</p>
+            <div className="images_slide">
+              <img src={house} />
+              <img src={house} />
+              <img src={house} />
+            </div>
+            <div className="buttons">
+              <button className="btn">Instant Book</button>
+              <button className="btn">Contact host</button>
+            </div>
+            <button onClick={closeModal} aria-label="close" className="x">
+              ❌
+            </button>
+          </div>
+        </div>
+      )}
+    </>
+  );
 }
 
 export default Listing;
