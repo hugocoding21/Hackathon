7d4aec58cab7a794b326250e437c7f7b94f5cc6d
Bascom3000 <112181817+Bascom3000@users.noreply.github.com>
2023-05-11 16:51:55 +0200

    about us page done

--- src/components/AboutUS/About.jsx
+++ src/components/AboutUS/About.jsx
@@ -7,17 +7,38 @@ const About = () => {
         <>
         <Navbar />
 
-        <div className='globalContainer'>
-            <div className='textContainer'>
-                <p>Welcome to our home exchange platform, a website created by four passionate web developer students. Our platform is designed to connect people who want to share their homes during holidays, making it easier and more affordable to travel around the world. With our platform, you can explore new places while staying in a comfortable home and experience the local culture in a way that's not possible with traditional accommodation options. Our platform is easy to use and offers a secure way for members to connect with each other and exchange their homes. We are committed to providing the best user experience possible and are constantly working to improve our platform. Join us today and start your next adventure!</p>
+        <div className='page'>
+            
+            <div className='globalContainer'>
+                <div className='textContainer'>
+                    <h2 className='titleAbout'>
+                    Welcome to our home exchange platform, a website created by four passionate web developer students.
+                    </h2>
+                    <br />
+                    <br />
+                    <p>Our platform is designed to connect people who want to share their homes during holidays, making it easier and more affordable to travel around the world. With our platform, you can explore new places while staying in a comfortable home and experience the local culture in a way that's not possible with traditional accommodation options. Our platform is easy to use and offers a secure way for members to connect with each other and exchange their homes. We are committed to providing the best user experience possible and are constantly working to improve our platform. Join us today and start your next adventure!</p>
+                </div>
+                <div className='titleTeam'>
+                    <h3>THE TEAM</h3>
+                </div>
+            <div className='teamContainer'>
+            
+                <div className='developer'>
+                    <h3>Khouloud Belkhir</h3>
+                </div>
+                <div className='developer'>
+                    <h3>Imene Ramdan</h3>
+                </div>
+                <div className='developer'>
+                    <h3>Hugo Hanocq</h3>
+                </div>
+                <div className='developer'>
+                    <h3>Florent Marin</h3>
+                </div>
+            
+            
             </div>
-        <div className='teamContainer'>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ducimus recusandae laborum excepturi alias velit ratione nihil aperiam minus sapiente! Iste nihil aperiam corrupti eum quae aliquam dolor quisquam molestiae? Ex.</p>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ducimus recusandae laborum excepturi alias velit ratione nihil aperiam minus sapiente! Iste nihil aperiam corrupti eum quae aliquam dolor quisquam molestiae? Ex.</p>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ducimus recusandae laborum excepturi alias velit ratione nihil aperiam minus sapiente! Iste nihil aperiam corrupti eum quae aliquam dolor quisquam molestiae? Ex.</p>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ducimus recusandae laborum excepturi alias velit ratione nihil aperiam minus sapiente! Iste nihil aperiam corrupti eum quae aliquam dolor quisquam molestiae? Ex.</p>
-
-        </div>
+            </div>
         </div>
         </>
     );
