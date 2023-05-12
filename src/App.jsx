dda7f51ae891c7aeb7548e382a9ccfe5961eebf1
hugocoding21 <118363369+hugocoding21@users.noreply.github.com>
2023-05-12 12:35:58 +0200

    dzdzz

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import About from "./components/AboutUS/About";
 import Filter from "./components/FilterDestination/Filter";
 import User from "./components/user/User";
 import Navbar from "./components/Navbar/Navbar";
+import { Form } from "react-router-dom";
+import Listing from "./components/Listing/Listing";
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Filter />}></Route>
+        <Route path="/listing" element={<Listing />}></Route>
+        <Route path="/Form" element={<Form />}></Route>
         <Route path="/about" element={<About />}></Route>
         <Route path="/user" element={<User />}></Route>
       </Routes>
