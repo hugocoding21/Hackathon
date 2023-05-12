fd8ea2673f25ebc485bf27b3882d23b8bb23df3f
hugocoding21 <118363369+hugocoding21@users.noreply.github.com>
2023-05-12 14:21:13 +0200

    dzdzd

--- src/components/Listing/Listings.js
+++ src/components/Listing/Listings.js
@@ -1,204 +1,229 @@
 export const houses = [
-      {
-        "id": 1,
-        "location": "27 Rue Sainte-Catherine, 33000 Bordeaux",
-        "city": "Bordeaux",
-        "description": "This charming one-bedroom apartment in the heart of Bordeaux is perfect for a romantic getaway. It's located in a historic building with plenty of character.",
-        "latitude": 44.8404,
-        "longitude": -0.5742,
-        "image": "https://images.pexels.com/photos/4857775/pexels-photo-4857775.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-      },
-      {
-        "id": 2,
-        "location": "3 Rue de la Republique, 69001 Lyon",
-        "city": "Lyon",
-        "description": "This modern two-bedroom apartment in Lyon's 1st arrondissement is perfect for a family or a group of friends. It features a spacious living room, a fully equipped kitchen, and contemporary decor.",
-        "latitude": 45.7679,
-        "longitude": 4.8344,
-        "image": "https://images.pexels.com/photos/439227/pexels-photo-439227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-      },
-      {
-        "id": 3,
-        "location": "4 Rue du Petit Saint-Jean, 34000 Montpellier",
-        "city": "Montpellier",
-        "description": "This cozy one-bedroom apartment in Montpellier's historic center is perfect for a solo traveler or a couple. It features a comfortable bed, a fully equipped kitchen, and a small balcony.",
-        "latitude": 43.6099,
-        "longitude": 3.8795,
-        "image": "https://images.pexels.com/photos/6301168/pexels-photo-6301168.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-      },
-      {
-        "id": 4,
-        "location": "19 Rue du Maréchal Foch, 06000 Nice",
-        "city": "Nice",
-        "description": "This stylish two-bedroom apartment in Nice's city center is perfect for a family or a group of friends. It features a spacious living room, a fully equipped kitchen, and a balcony with a view.",
-        "latitude": 43.7009,
-        "longitude": 7.2680,
-        "image": "https://images.pexels.com/photos/1643383/pexels-photo-1643383.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-      },
-      {
-        "id": 5,
-        "location": "13 Rue de la République, 13001 Marseille",
-        "city": "Marseille",
-        "description": "This bright one-bedroom apartment in Marseille's 1st arrondissement is perfect for a solo traveler or a couple. It features a comfortable bed, a fully equipped kitchen, and a small terrace.",
-        "latitude": 43.2965,
-        "longitude": 5.3718,
-        "image": "https://images.pexels.com/photos/6775268/pexels-photo-6775268.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-      },
-      {
-        "id": 6,
-        "location": "2 Rue Saint-Jacques, 44000 Nantes",
-        "city": "Nantes",
-        "description": "This spacious three-bedroom apartment in Nantes' historic center is perfect for a family or a group of friends. It features high ceilings, large windows, and beautiful decor.",
-        "latitude": 47.2173,
-        "longitude": -1.5534,
-        "image": "https://images.pexels.com/photos/6121088/pexels-photo-6121088.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-      },
-      {
-        "id":7,
-        "description": "This charming house features a spacious living room, cozy bedrooms, and a lovely garden. It's located in a quiet neighborhood just minutes from the city center.",
-        "city": "New York",
-        "image": 
-          "https://images.pexels.com/photos/7214166/pexels-photo-7214166.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-      },
-      {
-        "id":8,
-        "description": "Escape the hustle and bustle of the city and unwind in this cozy cottage by the lake. Perfect for couples or small families, this charming retreat has everything you need for a relaxing getaway.",
-        "city": "Seattle",
-        "image": 
-          "https://images.pexels.com/photos/5875839/pexels-photo-5875839.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-      },
-      {
-        "id":9,
-        "description": "Indulge in luxury in this stunning villa with breathtaking views of the city. With five bedrooms, a private pool, and an outdoor dining area, it's the perfect place for a memorable family vacation or a getaway with friends.",
-        "city": "Los Angeles",
-        "image": [
-          "https://images.pexels.com/photos/15173334/pexels-photo-15173334.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-        ]
-      },
-      {
-        "id":10,
-        "description": "This beautiful house features an open-concept living space, modern appliances, and a backyard with a barbecue grill. It's the perfect place for families or groups of friends to gather and enjoy the sunny weather.",
-        "city": "Miami",
-        "image": [
-          "https://images.pexels.com/photos/3016430/pexels-photo-3016430.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-        ]
-      },
-      {
-        "id":11,
-        "description": "Located in the heart of the city, this spacious apartment features high ceilings, large windows, and modern furnishings. It's within walking distance to the best restaurants, shops, and attractions in town.",
-        "city": "San Francisco",
-        "image": [
-          "https://images.pexels.com/photos/15242038/pexels-photo-15242038.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-        ]
-      },
-      {
-        "id":12,
-        "description": "This cozy cabin is nestled in the mountains and surrounded by beautiful scenery. It's the perfect place to unwind and enjoy nature with your loved ones.",
-        "city": "Asheville",
-        "image": [
-          "https://images.pexels.com/photos/2765436/pexels-photo-2765436.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-        ]
-      },
-      {
-        "id":13,
-        "description": "This elegant mansion features a grand entrance, luxurious furnishings, and a stunning pool. It's perfect for hosting events, such as weddings or corporate retreats.",
-        "city": "Houston",
-        "image": [
-          "https://images.pexels.com/photos/6957083/pexels-photo-6957083.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-        ]
-      },
-      {
-        "id":14,
-        "description": "This modern apartment boasts an open-concept living space, sleek design, and stunning views of the city. It's the perfect place for couples or solo travelers looking to experience the best of urban living.",
-        "city": "Toronto",
-        "image": [
-          "https://images.pexels.com/photos/6077368/pexels-photo-6077368.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-        ]
-      },
-      {
-        "city": "Cape Town",
-        "description": "Beautiful villa with stunning views of the ocean and mountains",
-        "location": {
-          "latitude": -33.9249,
-          "longitude": 18.4241
-        },
-        "image": "https://images.pexels.com/photos/6775268/pexels-photo-6775268.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-      },
-      {
-        "city": "Marrakech",
-        "description": "Traditional Moroccan house with a courtyard and pool",
-        "location": {
-          "latitude": 31.6295,
-          "longitude": -7.9811
-        },
-        "image": "https://images.pexels.com/photos/347141/pexels-photo-347141.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-      },
-      {
-        "city": "Nairobi",
-        "description": "Modern apartment with a balcony overlooking the city",
-        "location": {
-          "latitude": -1.2921,
-          "longitude": 36.8219
-        },
-        "image": "https://images.pexels.com/photos/7195853/pexels-photo-7195853.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-      },
-      {
-        "city": "Johannesburg",
-        "description": "Luxury penthouse with a private rooftop pool",
-        "location": {
-          "latitude": -26.2041,
-          "longitude": 28.0473
-        },
-        "image": "https://images.pexels.com/photos/15088502/pexels-photo-15088502.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-      },
-      {
-        "city": "Accra",
-        "description": "Spacious house with a large garden and outdoor seating area",
-        "location": {
-          "latitude": 5.6037,
-          "longitude": -0.187
-        },
-        "image": "https://images.pexels.com/photos/1101140/pexels-photo-1101140.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-      },
-      {
-        "city": "Lagos",
-        "description": "Charming bungalow with a bright and colorful interior",
-        "location": {
-          "latitude": 6.5244,
-          "longitude": 3.3792
-        },
-        "image": "https://images.pexels.com/photos/6758346/pexels-photo-6758346.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-      },
-      {
-        "city": "Dar es Salaam",
-        "description": "Comfortable apartment in a gated community with security",
-        "location": {
-          "latitude": -6.7924,
-          "longitude": 39.2083
-        },
-        "image": "https://images.pexels.com/photos/276724/pexels-photo-276724.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-      },
-      {
-        "city": "Kigali",
-        "description": "Cozy cottage in a quiet and peaceful neighborhood",
-        "location": {
-          "latitude": -1.9536,
-          "longitude": 30.0605
-        },
-        "image": "https://images.pexels.com/photos/4915971/pexels-photo-4915971.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-      },
-      {
-        "city": "Tunis",
-        "description": "Historic house with traditional Tunisian architecture",
-        "location": {
-          "latitude": 36.8065,
-          "longitude": 10.1815
-        },
-        "image": "https://images.pexels.com/photos/1013427/pexels-photo-1013427.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-      },
-    ]
-
-    
-
-
-
+  {
+    id: 1,
+    location: "27 Rue Sainte-Catherine, 33000 Bordeaux",
+    city: "Bordeaux",
+    description:
+      "This charming one-bedroom apartment in the heart of Bordeaux is perfect for a romantic getaway. It's located in a historic building with plenty of character.",
+    latitude: 44.8404,
+    longitude: -0.5742,
+    image:
+      "https://images.pexels.com/photos/4857775/pexels-photo-4857775.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  },
+  {
+    id: 2,
+    location: "3 Rue de la Republique, 69001 Lyon",
+    city: "Lyon",
+    description:
+      "This modern two-bedroom apartment in Lyon's 1st arrondissement is perfect for a family or a group of friends. It features a spacious living room, a fully equipped kitchen, and contemporary decor.",
+    latitude: 45.7679,
+    longitude: 4.8344,
+    image:
+      "https://images.pexels.com/photos/439227/pexels-photo-439227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  },
+  {
+    id: 3,
+    location: "4 Rue du Petit Saint-Jean, 34000 Montpellier",
+    city: "Montpellier",
+    description:
+      "This cozy one-bedroom apartment in Montpellier's historic center is perfect for a solo traveler or a couple. It features a comfortable bed, a fully equipped kitchen, and a small balcony.",
+    latitude: 43.6099,
+    longitude: 3.8795,
+    image:
+      "https://images.pexels.com/photos/6301168/pexels-photo-6301168.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  },
+  {
+    id: 4,
+    location: "19 Rue du Maréchal Foch, 06000 Nice",
+    city: "Nice",
+    description:
+      "This stylish two-bedroom apartment in Nice's city center is perfect for a family or a group of friends. It features a spacious living room, a fully equipped kitchen, and a balcony with a view.",
+    latitude: 43.7009,
+    longitude: 7.268,
+    image:
+      "https://images.pexels.com/photos/1643383/pexels-photo-1643383.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  },
+  {
+    id: 5,
+    location: "13 Rue de la République, 13001 Marseille",
+    city: "Marseille",
+    description:
+      "This bright one-bedroom apartment in Marseille's 1st arrondissement is perfect for a solo traveler or a couple. It features a comfortable bed, a fully equipped kitchen, and a small terrace.",
+    latitude: 43.2965,
+    longitude: 5.3718,
+    image:
+      "https://images.pexels.com/photos/6775268/pexels-photo-6775268.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  },
+  {
+    id: 6,
+    location: "2 Rue Saint-Jacques, 44000 Nantes",
+    city: "Nantes",
+    description:
+      "This spacious three-bedroom apartment in Nantes' historic center is perfect for a family or a group of friends. It features high ceilings, large windows, and beautiful decor.",
+    latitude: 47.2173,
+    longitude: -1.5534,
+    image:
+      "https://images.pexels.com/photos/6121088/pexels-photo-6121088.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  },
+  {
+    id: 7,
+    description:
+      "This charming house features a spacious living room, cozy bedrooms, and a lovely garden. It's located in a quiet neighborhood just minutes from the city center.",
+    city: "Rabat",
+    image:
+      "https://images.pexels.com/photos/7214166/pexels-photo-7214166.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  },
+  {
+    id: 8,
+    description:
+      "Escape the hustle and bustle of the city and unwind in this cozy cottage by the lake. Perfect for couples or small families, this charming retreat has everything you need for a relaxing getaway.",
+    city: "Seattle",
+    image:
+      "https://images.pexels.com/photos/5875839/pexels-photo-5875839.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  },
+  {
+    id: 9,
+    description:
+      "Indulge in luxury in this stunning villa with breathtaking views of the city. With five bedrooms, a private pool, and an outdoor dining area, it's the perfect place for a memorable family vacation or a getaway with friends.",
+    city: "Lille",
+    image: [
+      "https://images.pexels.com/photos/15173334/pexels-photo-15173334.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    ],
+  },
+  {
+    id: 10,
+    description:
+      "This beautiful house features an open-concept living space, modern appliances, and a backyard with a barbecue grill. It's the perfect place for families or groups of friends to gather and enjoy the sunny weather.",
+    city: "Miami",
+    image: [
+      "https://images.pexels.com/photos/3016430/pexels-photo-3016430.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    ],
+  },
+  {
+    id: 11,
+    description:
+      "Located in the heart of the city, this spacious apartment features high ceilings, large windows, and modern furnishings. It's within walking distance to the best restaurants, shops, and attractions in town.",
+    city: "Paris",
+    image: [
+      "https://images.pexels.com/photos/15242038/pexels-photo-15242038.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    ],
+  },
+  {
+    id: 12,
+    description:
+      "This cozy cabin is nestled in the mountains and surrounded by beautiful scenery. It's the perfect place to unwind and enjoy nature with your loved ones.",
+    city: "Asheville",
+    image: [
+      "https://images.pexels.com/photos/2765436/pexels-photo-2765436.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    ],
+  },
+  {
+    id: 13,
+    description:
+      "This elegant mansion features a grand entrance, luxurious furnishings, and a stunning pool. It's perfect for hosting events, such as weddings or corporate retreats.",
+    city: "Houston",
+    image: [
+      "https://images.pexels.com/photos/6957083/pexels-photo-6957083.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    ],
+  },
+  {
+    id: 14,
+    description:
+      "This modern apartment boasts an open-concept living space, sleek design, and stunning views of the city. It's the perfect place for couples or solo travelers looking to experience the best of urban living.",
+    city: "Toronto",
+    image: [
+      "https://images.pexels.com/photos/6077368/pexels-photo-6077368.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    ],
+  },
+  {
+    city: "Tokyo",
+    description:
+      "Beautiful villa with stunning views of the ocean and mountains",
+    location: {
+      latitude: -33.9249,
+      longitude: 18.4241,
+    },
+    image:
+      "https://images.pexels.com/photos/6775268/pexels-photo-6775268.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  },
+  {
+    city: "Marrakech",
+    description: "Traditional Moroccan house with a courtyard and pool",
+    location: {
+      latitude: 31.6295,
+      longitude: -7.9811,
+    },
+    image:
+      "https://images.pexels.com/photos/347141/pexels-photo-347141.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  },
+  {
+    city: "Nairobi",
+    description: "Modern apartment with a balcony overlooking the city",
+    location: {
+      latitude: -1.2921,
+      longitude: 36.8219,
+    },
+    image:
+      "https://images.pexels.com/photos/7195853/pexels-photo-7195853.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  },
+  {
+    city: "Johannesburg",
+    description: "Luxury penthouse with a private rooftop pool",
+    location: {
+      latitude: -26.2041,
+      longitude: 28.0473,
+    },
+    image:
+      "https://images.pexels.com/photos/15088502/pexels-photo-15088502.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  },
+  {
+    city: "Accra",
+    description: "Spacious house with a large garden and outdoor seating area",
+    location: {
+      latitude: 5.6037,
+      longitude: -0.187,
+    },
+    image:
+      "https://images.pexels.com/photos/1101140/pexels-photo-1101140.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  },
+  {
+    city: "Lagos",
+    description: "Charming bungalow with a bright and colorful interior",
+    location: {
+      latitude: 6.5244,
+      longitude: 3.3792,
+    },
+    image:
+      "https://images.pexels.com/photos/6758346/pexels-photo-6758346.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  },
+  {
+    city: "Séoul",
+    description: "Comfortable apartment in a gated community with security",
+    location: {
+      latitude: -6.7924,
+      longitude: 39.2083,
+    },
+    image:
+      "https://images.pexels.com/photos/276724/pexels-photo-276724.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  },
+  {
+    city: "Kigali",
+    description: "Cozy cottage in a quiet and peaceful neighborhood",
+    location: {
+      latitude: -1.9536,
+      longitude: 30.0605,
+    },
+    image:
+      "https://images.pexels.com/photos/4915971/pexels-photo-4915971.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  },
+  {
+    city: "Tunis",
+    description: "Historic house with traditional Tunisian architecture",
+    location: {
+      latitude: 36.8065,
+      longitude: 10.1815,
+    },
+    image:
+      "https://images.pexels.com/photos/1013427/pexels-photo-1013427.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  },
+];
