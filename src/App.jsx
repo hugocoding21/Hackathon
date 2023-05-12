17a46f3a5d85a85079c6b8a429c16862b68523b6
Bascom3000 <112181817+Bascom3000@users.noreply.github.com>
2023-05-12 09:06:59 +0200

    commit test weather api

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,32 @@
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
-
-
-import Form from "./components/Formulaire/Form";
-
-import Home from './components/Home/Home';
-
 import Filter from "./components/FilterDestination/Filter";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import About from "./components/AboutUS/About";
 import Navbar from "./components/Navbar/Navbar";
 
 
 
 function App() {
+const [meteoData, setMeteoData] = useState([])
+
+  useEffect(() => {
+      fetch('http://api.weatherapi.com/v1/forecast.json?key=29346aa7147e4bfea2d160503231105&q=Lille')
+      .then(response => response.json())
+      .then(data => setMeteoData(data))
+  }, [])
+
+  console.log(meteoData.current.temp_c)
   return (
+
+    
     <div className="page">
       <div className="nav">
-      <Form />
+      
         <Navbar />
+
       </div>
+      
       <Routes>
         <Route path="/" element={<Filter />}></Route>
         <Route path="/about" element={<About />}></Route>
